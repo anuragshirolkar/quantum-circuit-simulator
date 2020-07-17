eaf632efc3cc21e01bb310b210f47f41a2056b14
Anurag Shirolkar <ashirolkar@google.com>
2020-07-18 03:40:54 +0800

    immutablejs

--- src/wavefunction.ts
+++ src/wavefunction.ts
@@ -1,5 +1,11 @@
+import { Map } from "immutable"
+
 
 interface WaveFunction {
     map: Map<number, number>
     nBits: number
+}
+
+function collapse(index: number, value: number, waveFunction: WaveFunction): WaveFunction {
+    
 }
