3b7b152ffb128d310f868474364cb0e9dea2e972
Anurag Shirolkar <ashirolkar@google.com>
2020-07-18 16:40:34 +0800

    wavefunction collapse

---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+
+function sum(nums: number[]): number {
+    return nums.reduce((a,b) => a+b, 0)
+}
--- src/wavefunction.ts
+++ src/wavefunction.ts
@@ -1,4 +1,5 @@
 import { Map } from "immutable"
+import { nthBit } from "./bits"
 
 
 interface WaveFunction {
@@ -7,5 +8,12 @@ interface WaveFunction {
 }
 
 function collapse(index: number, value: number, waveFunction: WaveFunction): WaveFunction {
-    
+    var nonNormalizedCollapsedWaveFunctionMap = waveFunction.map
+        .filter((alpha, outcome) => nthBit(index, outcome) == value)
+    var denominator = Math.sqrt(sum(nonNormalizedCollapsedWaveFunctionMap.valueSeq().toArray().map(a => a*a)))
+    return {
+        ...waveFunction,
+        map: nonNormalizedCollapsedWaveFunctionMap.map(alpha => alpha/denominator)
+    }
 }
+
